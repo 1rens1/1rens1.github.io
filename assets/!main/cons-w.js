bbf6d6347d478fec53007786d2231aaf58460067
1rens1 <rein.satyawarman@gmail.com>
2021-07-08 16:12:35 +0700

    Added autograph

--- assets/!main/cons-w.js
+++ assets/!main/cons-w.js
@@ -1,12 +1,1 @@
-console.error("Oh look, a highlighted red text.");
-console.log(
-    "  %cWARNING!",
-    "color: red; background: yellow; font-size: 35px; font-family: Consolas, Monospace; text-shadow: 2.5px 2.5px 0px #ffaf00; text-decoration: underline; text-decoration-thickness: .2rem",
-);
-console.log(
-    "%c  Using this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.\nDo not enter or paste code that you do not understand.",
-    "font-size: 18px; font-family: Consolas, Monospace;"
-);
-console.log(
-    "For more information, goto\nhttps://en.wikipedia.org/wiki/Self-XSS"
-);
+console.error("Oh look, a highlighted red text.");console.log("  %c WARNING! ","color: red; background: yellow; font-size: 35px; font-family: Consolas, Monospace; text-shadow: 2.5px 2.5px 0px #ffaf00; text-decoration-thickness: 0.1rem;",);console.log("%c  Using this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.\nDo not enter or paste code that you do not understand.","font-size: 18px; font-family: Consolas, Monospace;");console.log("For more information, goto\nhttps://en.wikipedia.org/wiki/Self-XSS");console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
